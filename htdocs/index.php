bf30ccf56df11d0cfbe1e69594e255ca986a2b96
pandur <pandur>
2002-08-19 18:14:58 +0000

    Initial revision

---
+++ htdocs/index.php
@@ -0,0 +1,38 @@
+<html>
+<title>CALM</title>
+<h1>CALM - Companywide Aggregated License Management</h1>
+
+
+<table width="100%">
+<tr><td width="15%" bgcolor="#c0c0ff" valign="top">
+
+This project is hosted by
+<a href="http://developer.berlios.de">
+<img src="http://developer.berlios.de/sflogo.php?group_id=0&type=1" width="124" height="32" border="0" alt="BerliOS Logo"></A>
+
+<hr>
+<!-----------------------------MENU-------------------->
+
+</td><td width="*" valign="top">
+<!-----------------------------CONTENT----------------->
+
+<h2>Aim</h2>
+
+CALM is aimed to create a license management system that solves the users problems (not the monetary ones of the software industry), so it has several targets:
+
+<ul>
+<li>show up unused licenses: they may be reused where needed
+<li>built on web technology: every admin in the intranet is able to connect to the central license pool, no need for complicated papers
+<li>built with OpenSource Software, so that it does not introduce potential license problems of its own
+</ul>
+
+<h2>Status</h2>
+
+CALM is currently in a pre-alpha status. It will need some time to create a prototype that can be used outside.
+
+
+</td></tr>
+</table>
+
+
+</html>
